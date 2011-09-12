38fb82912d2d15e9603d1ee043520a80ab6c4eff
lprichar <lprichar@localhost>
2011-09-12 16:00:15 -0400

    Fixed failing unit tests

---
+++ SirenOfShame.Lib/Device/ISirenOfShameDevice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using SirenOfShame.Lib.Device.SdCardFileSystem;
+
+namespace SirenOfShame.Lib.Device
+{
+    public interface ISirenOfShameDevice
+    {
+        bool IsConnected { get; }
+        IEnumerable<AudioPattern> AudioPatterns { get; }
+        IEnumerable<LedPattern> LedPatterns { get; }
+        int Version { get; }
+        HardwareType HardwareType { get; }
+        event EventHandler Connected;
+        event EventHandler Disconnected;
+        void SetAudio(AudioPattern pattern, TimeSpan? duration);
+        void SetLight(LedPattern pattern, TimeSpan? duration);
+        void WndProc(ref Message message);
+        void UploadCustomPatterns(IEnumerable<UploadAudioPattern> audioPatterns, IEnumerable<UploadLedPattern> ledPatterns, Action<int> progressFunc);
+        bool TryConnect();
+        void PerformFirmwareUpgrade(Stream hexFileStream, Action<int> progressFunc);
+        SirenOfShameInfo ReadDeviceInfo();
+        void Disconnect();
+        void ManualControl(ManualControlData manualControlData);
+    }
+}
---
+++ SirenOfShame.Lib/Helpers/XElementHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace SirenOfShame.Lib.Helpers
+{
+    public static class XElementHelpers
+    {
+        public static string AttributeValueOrDefault(this XElement elem, XName name)
+        {
+            var attr = elem.Attribute(name);
+            return attr == null ? null : attr.Value;
+        }
+
+        public static string AttributeValue(this XElement elem, XName name)
+        {
+            var attr = elem.Attribute(name);
+            if (attr == null)
+            {
+                throw new Exception("Attribute '" + name + "' does not exist");
+            }
+            return attr.Value;
+        }
+
+        public static string ElementValueOrDefault(this XElement elem, XName name)
+        {
+            var childEleme = elem.Element(name);
+            return childEleme == null ? null : childEleme.Value;
+        }
+
+        public static bool? ElementValueAsBool(this XElement elem, XName name, bool? defaultVal)
+        {
+            var val = ElementValueOrDefault(elem, name);
+            if (string.IsNullOrWhiteSpace(val))
+            {
+                return defaultVal;
+            }
+            bool result;
+            if (bool.TryParse(val, out result))
+            {
+                return result;
+            }
+            throw new Exception("Could not parse or read element " + name);
+        }
+    }
+}
