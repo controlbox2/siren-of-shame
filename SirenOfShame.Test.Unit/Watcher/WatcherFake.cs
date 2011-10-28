6df5df19ad6c74a0b8dcd570a15c972ab5fbe0de
lrichardson <lrichardson@localhost>
2011-10-28 17:44:04 -0400

    #129 Notify text on minimize/close is annoying

---
+++ SirenOfShame.Test.Unit/Watcher/WatcherFake.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SirenOfShame.Lib.Exceptions;
+using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
+
+namespace SirenOfShame.Test.Unit.Watcher
+{
+    public class WatcherFake : WatcherBase
+    {
+        public WatcherFake(SirenOfShameSettings settings)
+            : base(settings)
+        {
+        }
+
+        protected override IList<BuildStatus> GetBuildStatus()
+        {
+            return new List<BuildStatus>();
+        }
+
+        public override void Dispose()
+        {
+            // do nothing
+        }
+
+        public override void StartWatching()
+        {
+            // do nothing
+        }
+
+        public override void StopWatching()
+        {
+            // do nothing
+        }
+
+        public new void InvokeServerUnavailable(ServerUnavailableException ex)
+        {
+            base.InvokeServerUnavailable(ex);
+        }
+
+        public new void InvokeStatusChecked(IList<BuildStatus> args) {
+            base.InvokeStatusChecked(args);
+        }
+    }
+}
---
+++ TeamCityServices/TeamCityBuildStatus.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+using SirenOfShame.Lib;
+using SirenOfShame.Lib.Helpers;
+using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
+using log4net;
+
+namespace TeamCityServices
+{
+    /*
+<build id="2" number="2" status="SUCCESS" href="/httpAuth/app/rest/builds/id:2" webUrl="http://vmxp:8080/viewLog.html?buildId=2&buildTypeId=bt2" personal="false" history="false" pinned="false">
+    <statusText>Success</statusText>
+    <buildType id="bt2" name="Test Project 1 Build" href="/httpAuth/app/rest/buildTypes/id:bt2" projectName="Test Project 1" projectId="project2" webUrl="http://vmxp:8080/viewType.html?buildTypeId=bt2"/>
+    <startDate>20110606T040133+0300</startDate>
+    <finishDate>20110606T040142+0300</finishDate>
+    <agent name="vmxp" id="1" href="/httpAuth/app/rest/agents/id:1"/>
+    <tags/>
+    <properties/>
+    <revisions>
+        <revision display-version="0f76111b776a">
+            <vcs-root href="/httpAuth/app/rest/vcs-roots/id:1" name="mercurial: C:\hgrepo\TestProject1"/>
+        </revision>
+    </revisions>
+    <changes href="/httpAuth/app/rest/changes?build=id:2" count="0"/>
+    <relatedIssues/>
+</build>
+     */
+    public class TeamCityBuildStatus : BuildStatus
+    {
+        private static readonly ILog _log = MyLogManager.GetLogger(typeof(TeamCityBuildStatus));
+
+        // todo: parse this better
+        private static DateTime GetTeamCityDate(string startedTimeStr)
+        {
+            startedTimeStr = startedTimeStr.Replace('T', ' ');
+            int i = startedTimeStr.IndexOf('+');
+            if (i > 0)
+            {
+                startedTimeStr = startedTimeStr.Substring(0, i);
+            }
+            i = startedTimeStr.IndexOf('-');
+            if (i > 0)
+            {
+                startedTimeStr = startedTimeStr.Substring(0, i);
+            }
+            return DateTime.ParseExact(startedTimeStr, "yyyyMMdd HHmmss", CultureInfo.InvariantCulture);
+        }
+
+        public TeamCityBuildStatus(BuildDefinitionSetting buildDefinitionSetting, XDocument buildResultXDoc, XDocument changeResultXDoc)
+        {
+            if (changeResultXDoc != null)
+            {
+                Comment = changeResultXDoc.Descendants("comment").First().Value;
+                RequestedBy = changeResultXDoc.Root.AttributeValueOrDefault("username");
+            }
+
+            string status = buildResultXDoc.Root.AttributeValueOrDefault("status");
+            string startedTimeStr = buildResultXDoc.Root.ElementValueOrDefault("startDate");
+            string finishedTimeStr = buildResultXDoc.Root.ElementValueOrDefault("finishDate");
+
+            BuildDefinitionId = buildDefinitionSetting.Id;
+            Name = buildDefinitionSetting.Name;
+            StartedTime = GetTeamCityDate(startedTimeStr);
+            if (string.IsNullOrEmpty(finishedTimeStr))
+            {
+                BuildStatusEnum = BuildStatusEnum.InProgress;
+            }
+            else
+            {
+                FinishedTime = GetTeamCityDate(finishedTimeStr);
+                BuildStatusEnum = ToBuildStatusEnum(status);
+            }
+        }
+
+        private BuildStatusEnum ToBuildStatusEnum(string status)
+        {
+            switch (status)
+            {
+                case "SUCCESS":
+                    return BuildStatusEnum.Working;
+                case "FAILURE":
+                    return BuildStatusEnum.Broken;
+                default:
+                    _log.Warn("Unknown build status: " + status);
+                    return BuildStatusEnum.Unknown;
+            }
+        }
+    }
+}
