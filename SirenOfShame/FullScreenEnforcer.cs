3db7b7fcc54a070fc38a158a92d089cc1c397a94
lprichar <lprichar@localhost>
2011-12-01 21:29:40 -0500

    Logs Hudson bugs better

---
+++ SirenOfShame/FullScreenEnforcer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using SirenOfShame.Lib;
+using SirenOfShame.Lib.Exceptions;
+using log4net;
+
+namespace SirenOfShame
+{
+    public partial class FullScreenEnforcer : Form
+    {
+        private static readonly ILog Log = MyLogManager.GetLogger(typeof(FullScreenEnforcer));
+        private readonly bool _screenSaverActive;
+
+        public FullScreenEnforcer()
+        {
+            InitializeComponent();
+            try
+            {
+                var regVal = (Registry.GetValue("HKEY_CURRENT_USER\\Control Panel\\Desktop", "ScreenSaveActive", "1") ?? "1").ToString();
+                if (regVal == "1") regVal = "true";
+                if (regVal == "0") regVal = "false";
+                _screenSaverActive = bool.Parse(regVal);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Could not get screen saver value", ex);
+                _screenSaverActive = true;
+            }
+        }
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            ResetScreenSaverSettings();
+            base.OnVisibleChanged(e);
+        }
+
+        private void ResetScreenSaverSettings()
+        {
+            try
+            {
+                if (Visible)
+                {
+                    Registry.SetValue("HKEY_CURRENT_USER\\Control Panel\\Desktop", "ScreenSaveActive", "0");
+                }
+                else
+                {
+                    Registry.SetValue("HKEY_CURRENT_USER\\Control Panel\\Desktop", "ScreenSaveActive",
+                                      _screenSaverActive ? "1" : "0");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Could not set screen saver", ex);
+            }
+        }
+
+        private string TimeSpanAsText(TimeSpan timespan, OvertimeStatus overtimeStatus)
+        {
+            if (overtimeStatus == OvertimeStatus.Normal)
+            {
+                return ((int)timespan.TotalMinutes).ToString();
+            }
+            return TimeboxEnforcer.DurationAsText(timespan);
+        }
+
+        public void UpdateText(TimeSpan timespan, OvertimeStatus overtimeStatus)
+        {
+            _label.Text = TimeSpanAsText(timespan, overtimeStatus);
+            _label.ForeColor = GetForecolor(overtimeStatus);
+        }
+
+        private Color GetForecolor(OvertimeStatus overtimeStatus)
+        {
+            switch (overtimeStatus)
+            {
+                case OvertimeStatus.Normal:
+                    return Color.White;
+                case OvertimeStatus.Warning:
+                    return Color.Yellow;
+                case OvertimeStatus.Overtime:
+                    return Color.Red;
+                default:
+                    throw new SosException("Unknown status: " + overtimeStatus);
+            }
+        }
+
+        private void FullScreenEnforcerKeyDown(object sender, KeyEventArgs e)
+        {
+            Hide();
+        }
+
+        private void LabelClick(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void FullScreenEnforcer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ResetScreenSaverSettings();
+        }
+    }
+}
---
+++ SirenOfShame/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using log4net;
+using Microsoft.VisualBasic.ApplicationServices;
+using SirenOfShame.Lib;
+using SirenOfShame.Lib.Device;
+using SirenOfShame.Lib.Helpers;
+using SirenOfShame.Lib.Services;
+
+namespace SirenOfShame
+{
+    public class Program : WindowsFormsApplicationBase
+    {
+        private static ILog _log;
+        private bool _startMinimized;
+        private static MainForm _form;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            try
+            {
+                RealMain(args);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Siren of Shame Startup Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static void RealMain(string[] args)
+        {
+            try
+            {
+                _log = MyLogManager.GetLogger(typeof(Program));
+                Application.ThreadException += ApplicationThreadException;
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                new Program().Run(args);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+                throw;
+            }
+        }
+
+        private static void ApplicationThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            _log.Error("Global error", e.Exception);
+            ExceptionMessageBox.Show(null, "Drat", "Something crazy just happened.", e.Exception);
+        }
+
+        public static MainForm Form
+        {
+            get { return _form; }
+        }
+
+        public Program()
+        {
+            IsSingleInstance = true;
+            EnableVisualStyles = true;
+            StartupNextInstance += SirenOfShameApplication_StartupNextInstance;
+        }
+
+        private void SirenOfShameApplication_StartupNextInstance(object sender, StartupNextInstanceEventArgs e)
+        {
+            MainForm.Show();
+            MainForm.WindowState = FormWindowState.Normal;
+            e.BringToForeground = true;
+        }
+
+        protected override bool OnInitialize(ReadOnlyCollection<string> args)
+        {
+            bool mockSoS = false;
+            bool showSplash = true;
+            _startMinimized = false;
+            if (args.Any(s => s == "/nosplash"))
+            {
+                showSplash = false;
+            }
+            if (args.Any(s => s == "/mocksos"))
+            {
+                mockSoS = true;
+            }
+            if (args.Any(s => s == "/min"))
+            {
+                _startMinimized = true;
+            }
+
+            // todo: we shouldn't need to do this
+            IocContainer.Instance.Register(typeof(AudioFileService), new AudioFileService());
+            IocContainer.Instance.Register(typeof(LedFileService), new LedFileService());
+
+            if (mockSoS)
+            {
+                IocContainer.Instance.Register(typeof(ISirenOfShameDevice), new MockSirenOfShameDevice());
+            }
+            else
+            {
+                IocContainer.Instance.Register(typeof(ISirenOfShameDevice), new SirenOfShameDevice());
+            }
+            IocContainer.Instance.GetExport<ISirenOfShameDevice>().TryConnect();
+
+            IocContainer.Instance.TryLogAssemblyVersions();
+
+            if (showSplash)
+            {
+                SplashScreen = new SplashScreen();
+            }
+            return base.OnInitialize(args);
+        }
+
+        protected override void OnCreateMainForm()
+        {
+            MainForm = _form = new MainForm();
+            if (_startMinimized)
+            {
+                MainForm.WindowState = FormWindowState.Minimized;
+            }
+            base.OnCreateMainForm();
+        }
+    }
+}
