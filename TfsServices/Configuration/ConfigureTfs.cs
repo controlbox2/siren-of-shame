3ca0f97e9e1f35ce5bfae0fd34c608a8ea736076
lprichar <lprichar@localhost>
2011-12-09 14:46:23 -0500

    Deploy 1.2.0

---
+++ TfsServices/Configuration/ConfigureTfs.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using log4net;
+using SirenOfShame.Lib;
+using SirenOfShame.Lib.ServerConfiguration;
+using SirenOfShame.Lib.Settings;
+
+namespace TfsServices.Configuration
+{
+    public partial class ConfigureTfs : ConfigureServerBase
+    {
+        private readonly TfsCiEntryPoint _tfsCiEntryPoint;
+        private static readonly ILog _log = MyLogManager.GetLogger(typeof(ConfigureTfs));
+        private readonly CiEntryPointSetting _ciEntryPointSetting;
+        
+        public ConfigureTfs(SirenOfShameSettings settings, TfsCiEntryPoint tfsCiEntryPoint, CiEntryPointSetting ciEntryPointSetting)
+            : base(settings)
+        {
+            _tfsCiEntryPoint = tfsCiEntryPoint;
+            InitializeComponent();
+            _ciEntryPointSetting = ciEntryPointSetting;
+            _url.Text = _ciEntryPointSetting.Url;
+            DataBindAsync();
+        }
+
+        private void DataBindAsync()
+        {
+            if (_ciEntryPointSetting.Url == null)
+            {
+                // ToDo: dynamically try to find the url
+                return;
+            }
+
+            _buildConfigurations.Nodes.Clear();
+            _buildConfigurations.Nodes.Add("Contacting server...");
+
+            Thread t = new Thread(DataBind) {IsBackground = true};
+            t.Start();
+        }
+
+        private void DataBind()
+        {
+            try
+            {
+                using (var tfs = new MyTfsServer(_ciEntryPointSetting.Url))
+                {
+                    IList<TreeNode> projectCollectionNodes = new List<TreeNode>();
+
+                    foreach (var teamProjectCollection in tfs.ProjectCollections)
+                    {
+                        TreeNode projectCollectionNode = new TreeNode(teamProjectCollection.Name);
+                        foreach (var project in teamProjectCollection.Projects)
+                        {
+                            var projectNode = projectCollectionNode.Nodes.Add(project.Name);
+                            foreach (var buildDefinition in project.BuildDefinitions)
+                            {
+                                var buildDefinitionSetting = _ciEntryPointSetting.FindAddBuildDefinition(buildDefinition, _tfsCiEntryPoint.Name);
+                                projectNode.Nodes.Add(buildDefinition.GetAsNode(buildDefinitionSetting.Active));
+                            }
+                        }
+                        projectCollectionNodes.Add(projectCollectionNode);
+                    }
+
+                    Invoke(() => _buildConfigurations.Nodes.Clear());
+                    Invoke(() => _buildConfigurations.Nodes.AddRange(projectCollectionNodes.ToArray()));
+
+                }
+                Settings.Save();
+            }
+            catch (Exception ex)
+            {
+                Invoke(() => _buildConfigurations.Nodes.Clear());
+                _log.Error("Failed to connect to server", ex);
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GoClick(object sender, EventArgs e)
+        {
+            _ciEntryPointSetting.Url = _url.Text;
+            Settings.Save();
+            DataBindAsync();
+        }
+
+        private void BuildConfigurationsAfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Tag == null) return;
+            var buildDefinitionId = (string)e.Node.Tag;
+            _ciEntryPointSetting.GetBuildDefinition(buildDefinitionId).Active = e.Node.Checked;
+            Settings.Save();
+        }
+
+        private void UrlTextChanged(object sender, EventArgs e)
+        {
+            _ciEntryPointSetting.Url = _url.Text;
+            Settings.Save();
+        }
+    }
+}
