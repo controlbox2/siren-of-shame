eec99daca03d95197fa9000dcf746095b88653f0
lprichar <lprichar@localhost>
2012-02-25 23:00:31 -0500

    Fixed #155, CruiseControl.Net wasn't returning started date (+ new cc tests)

---
+++ SirenOfShame.Test.Unit/Settings/SirenOfShameSettingsTest.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable InconsistentNaming
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SirenOfShame.Test.Unit.Watcher;
+
+namespace SirenOfShame.Test.Unit.Settings
+{
+    [TestClass]
+    public class SirenOfShameSettingsTest
+    {
+        [TestMethod]
+        public void Upgrade_NullVersion_UpgradedToMostRecent()
+        {
+            var settings = new SirenOfShameSettingsFake
+                               {
+                                   Version = null
+                               };
+            settings.DoUpgrade();
+            Assert.AreEqual(3, settings.Version);
+        }
+        
+        [TestMethod]
+        public void Upgrade_FutureVersion_NotDowngraded()
+        {
+            var settings = new SirenOfShameSettingsFake
+                               {
+                                   Version = int.MaxValue
+                               };
+            settings.DoUpgrade();
+            Assert.AreEqual(int.MaxValue, settings.Version);
+        }
+    }
+}
