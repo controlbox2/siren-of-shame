885d0a73b2777db7828bf03039c1d1a19891e7fb
lprichar <lprichar@localhost>
2012-05-09 17:09:54 -0400

    Should resolve Issue #14

---
+++ Releases/copyeverything.bat
@@ -0,0 +1,10 @@
+copy ..\SirenOfShame\bin\SirenOfShame.exe .\BuildArtifacts\%1
+copy ..\SirenOfShame\bin\SirenOfShame.Lib.dll .\BuildArtifacts\%1
+mkdir BuildArtifacts\%1\Plugins
+copy ..\SirenOfShame\bin\Plugins\BambooServices.dll .\BuildArtifacts\%1\Plugins
+copy ..\SirenOfShame\bin\Plugins\CruiseControlNetServices.dll .\BuildArtifacts\%1\Plugins
+copy ..\SirenOfShame\bin\Plugins\HudsonServices.dll .\BuildArtifacts\%1\Plugins
+copy ..\SirenOfShame\bin\Plugins\TeamCityServices.dll .\BuildArtifacts\%1\Plugins
+copy ..\SirenOfShame\bin\Plugins\TfsServices.dll .\BuildArtifacts\%1\Plugins
+copy ..\SirenOfShame\bin\Plugins\UsbLib.dll .\BuildArtifacts\%1\Plugins
+copy ..\Libs\ZedGraph.dll .\BuildArtifacts\%1
