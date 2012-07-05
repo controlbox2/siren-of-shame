1594cd2941de1a2a3903319039c4c9af49c390be
lrichardson <lrichardson@localhost>
2012-07-05 10:29:43 -0400

    Close button prettier

---
+++ SirenOfShame.Lib/ICIEntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using SirenOfShame.Lib.ServerConfiguration;
+using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
+
+namespace SirenOfShame.Lib
+{
+    public interface ICiEntryPoint
+    {
+        ConfigureServerBase CreateConfigurationWindow(SirenOfShameSettings settings, CiEntryPointSetting ciEntryPointSetting);
+        string Name { get; }
+        string DisplayName { get; }
+        WatcherBase GetWatcher(SirenOfShameSettings settings);
+    }
+}
