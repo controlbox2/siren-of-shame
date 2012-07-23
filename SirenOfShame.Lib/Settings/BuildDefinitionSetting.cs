f2a5d42b233ff142a0cd5142c8323a2e0726f6b2
lrichardson <lrichardson@localhost>
2012-07-23 10:14:00 -0400

    Catch TFS VerisonControlExcepitons

---
+++ SirenOfShame.Lib/Settings/BuildDefinitionSetting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SirenOfShame.Lib.Watcher;
+
+namespace SirenOfShame.Lib.Settings {
+	[Serializable]
+	public class BuildDefinitionSetting {
+		public BuildDefinitionSetting()
+		{
+		    People = new List<string>();
+		}
+
+        public BuildDefinitionSetting(MyBuildDefinition buildDefinition, string buildServer) : this()
+        {
+			Active = false;
+			Id = buildDefinition.Id;
+			Name = buildDefinition.Name;
+            AffectsTrayIcon = true;
+            BuildServer = buildServer;
+        }
+
+		public bool Active { get; set; }
+		public string Id { get; set; }
+		public string Name { get; set; }
+        public bool AffectsTrayIcon { get; set; }
+        public List<string> People { get; set; }
+	    public string BuildServer { get; set; }
+
+	    public BuildStatus AsUnknownBuildStatus()
+	    {
+	        return new BuildStatus
+	        {
+	            BuildStatusEnum = BuildStatusEnum.Unknown,
+	            BuildDefinitionId = Id,
+	            Name = Name,
+                StartedTime = null
+	        };
+	    }
+
+        public bool ContainsPerson(BuildStatus buildStatus)
+        {
+            if (buildStatus == null) return false;
+            return People.Any(p => p == buildStatus.RequestedBy);
+        }
+	}
+}
---
+++ SirenOfShame/ViewUser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using SirenOfShame.Lib.Services;
+using SirenOfShame.Lib.Settings;
+
+namespace SirenOfShame
+{
+    public partial class ViewUser : UserControl
+    {
+        private SirenOfShameSettings _settings;
+        public event CloseViewUser OnClose;
+        
+        public ViewUser()
+        {
+            InitializeComponent();
+        }
+
+        public void Initilaize(SirenOfShameSettings settings)
+        {
+            _settings = settings;
+        }
+
+        private void CloseButtonClick(object sender, EventArgs e)
+        {
+            OnClose(this, new CloseViewUserArgs());
+        }
+
+        public void SetUser(PersonSetting personSetting)
+        {
+            _userName.Text = personSetting.GetBothDisplayAndRawNames();
+            flowLayoutPanel1.Controls.Clear();
+
+            foreach (var achievementLookup in AchievementSetting.AchievementLookups)
+            {
+                var label = new Label();
+                label.Text = achievementLookup.Name;
+                bool hasUserAchieved = personSetting.Achievements.Any(i => i.AchievementId == (int)achievementLookup.Id);
+                var cloneFrom = hasUserAchieved ? _obtainedTemplate : _unobtainedTemplate;
+                label.Font = cloneFrom.Font;
+                label.ForeColor = cloneFrom.ForeColor;
+                label.BackColor = cloneFrom.BackColor;
+                label.BorderStyle = cloneFrom.BorderStyle;
+                label.AutoSize = cloneFrom.AutoSize;
+                label.Margin = cloneFrom.Margin;
+                flowLayoutPanel1.Controls.Add(label);
+                toolTip1.SetToolTip(label, achievementLookup.Description);
+            }
+        }
+
+        private void _viewAllAchievements_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(SosOnlineService.SOS_URL + "/Achievements");
+        }
+    }
+
+    public delegate void CloseViewUser(object sender, CloseViewUserArgs args);
+
+    public class CloseViewUserArgs
+    {
+    }
+}
